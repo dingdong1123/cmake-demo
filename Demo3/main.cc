5aea6026b5fd232226d459aad541b3f95c5d4f89
peter.ding <94023888@qq.com>
2022-07-27 23:31:23 +0800

    debug cmake

--- Demo3/main.cc
+++ Demo3/main.cc
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "math/MathFunctions.h"
+#include "lib/math/MathFunctions.h"
+#include "lib/hello/Hello.h"
 
 int main(int argc, char *argv[])
 {
@@ -11,6 +12,8 @@ int main(int argc, char *argv[])
     double base = atof(argv[1]);
     int exponent = atoi(argv[2]);
     double result = power(base, exponent);
+    printf("%g ^ %d is %g\n", base, exponent, result);
+    result = hello(base, exponent);
     printf("%g ^ %d is %g\n", base, exponent, result);
     return 0;
 }
